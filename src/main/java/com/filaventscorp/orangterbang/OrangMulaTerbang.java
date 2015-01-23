ef3b30031847d0050bb0942aa98069752d3bd0a5
Alif <alifaziz@gmail.com>
2015-01-24 02:24:59 +0800

    updates

--- src/main/java/com/filaventscorp/orangterbang/OrangMulaTerbang.java
+++ src/main/java/com/filaventscorp/orangterbang/OrangMulaTerbang.java
@@ -4,6 +4,7 @@ import com.ECS.client.jax.*;
 import de.malkusch.amazon.ecs.ProductAdvertisingAPI;
 import de.malkusch.amazon.ecs.configuration.Configuration;
 import de.malkusch.amazon.ecs.configuration.PropertiesConfiguration;
+import de.malkusch.amazon.ecs.exception.RequestException;
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
@@ -39,27 +40,58 @@ public class OrangMulaTerbang {
             "i want new bag"};
 
     private static String replies[] = {
-            " make a surprise, get a nice bag as a present for him, buy it now > ",
-            " make him happy by getting a present. He might like this > ",
-            " he probably loves this nice bag > ",
-            " nice leather bag for him. Make him smile. Buy it now > ",
-            " Best deal for a nice looking bag for man > "
+            " make a surprise, get a nice bag for someone you love > ",
+            " make someone happy. The person might like this > ",
+            " you probably love this nice bag > ",
+            " nice leather bag for him. Make someone smile > ",
+            " Best deal for a nice looking bag > "
     };
 
     public static void main(String[] args) throws IOException, TwitterException, de.malkusch.amazon.ecs.exception.RequestException, NullPointerException {
+        try {
+            facade();
+        } catch (TwitterException e) {
+            facade();
+        }
+    }
 
+    public static void facade() throws IOException, TwitterException, de.malkusch.amazon.ecs.exception.RequestException, NullPointerException {
         AMAZON_API = configureAmazonAPI();
         TWITTER_API = configureTwitterAuth();
         rnd = new Random();
 
-
         // ----- TwITTER ---- //
-        Query query = new Query(keywords[rnd.nextInt(keywords.length)]);
+        //Query query = new Query(keywords[rnd.nextInt(keywords.length)]);
+        Query query = new Query(getTrendingTags());
         QueryResult result = TWITTER_API.search(query);
 
-
+        // Get new Tweets collection from search
         Status sts = result.getTweets().get(rnd.nextInt(result.getTweets().size()));
+        favoritesAllMentions();
+        getRandomProductsFromAmazon(sts, getTrendingTags());
+        tweetRandomProductToUser(sts);
+    }
 
+    public static String getTrendingTags() throws TwitterException {
+        Trends trends = TWITTER_API.getPlaceTrends(23424977);
+        Trend[] trend = trends.getTrends();
+        /*for (int i = 0; i < trend.length; i++) {
+            trend[i].getName();
+            System.out.println(trend[i].getName());
+        }*/
+        return trend[0].getName();
+    }
+
+    public static void tweetRandomProductToUser(Status sts) throws TwitterException {
+        StatusUpdate statReply = new StatusUpdate(stats);
+        statReply.setInReplyToStatusId(sts.getId());
+
+        //System.out.println(stats);
+        //System.out.println(statReply.getStatus());
+        TWITTER_API.updateStatus(statReply);
+    }
+
+    public static void getRandomProductsFromAmazon(Status sts, String trendingTags) throws RequestException {
 
         // ---- AMAZON ------ //
         ItemSearchRequest itemSearchRequest = new ItemSearchRequest();
@@ -76,29 +108,31 @@ public class OrangMulaTerbang {
         for (Offer offer : selectItem.getOffers().getOffer()) {
             for (OfferListing offerList : offer.getOfferListing()) {
                 //System.out.println("Price: " + offerList.getPrice().getFormattedPrice());
-                stats = "@" + sts.getUser().getScreenName() + replies[rnd.nextInt(replies.length)] + selectItem.getDetailPageURL() + " ONLY " + offerList.getPrice().getFormattedPrice();
-                if(offerList.getSalePrice() != null) {
+                stats = "@" + sts.getUser().getScreenName() + replies[rnd.nextInt(replies.length)] + selectItem.getDetailPageURL() + " ONLY " + offerList.getPrice().getFormattedPrice() + " Please RT. TQ. " + trendingTags;
+                if (offerList.getSalePrice() != null) {
                     //System.out.println("Sale Price: " + offerList.getSalePrice().getFormattedPrice());
                 }
-                if(offerList.getAmountSaved() != null) {
+                if (offerList.getAmountSaved() != null) {
                     //System.out.println("Amount Saved: " + offerList.getAmountSaved().getFormattedPrice());
                 }
-                if(offerList.getPercentageSaved() != null) {
+                if (offerList.getPercentageSaved() != null) {
                     //.out.println("%" + offerList.getPercentageSaved() + " saved!");
-                    stats = "@" + sts.getUser().getScreenName() + replies[rnd.nextInt(replies.length)] + selectItem.getDetailPageURL() + " ONLY " + offerList.getPrice().getFormattedPrice() + " [" + offerList.getPercentageSaved() + "% SAVED!]";
+                    stats = "@" + sts.getUser().getScreenName() + replies[rnd.nextInt(replies.length)] + selectItem.getDetailPageURL() + " ONLY " + offerList.getPrice().getFormattedPrice() + " [" + offerList.getPercentageSaved() + "% SAVED!] Please RT. TQ. " + trendingTags;
                 }
                 //System.out.println("Availability: " + offerList.getAvailability());
             }
         }
+    }
 
-        StatusUpdate statReply = new StatusUpdate(stats);
-        statReply.setInReplyToStatusId(sts.getId());
-
-        //System.out.println(stats);
-        //System.out.println(statReply.getStatus());
-        TWITTER_API.updateStatus(statReply);
-
-
+    public static void favoritesAllMentions() throws TwitterException {
+        // Find all mentions and FAVOURITE THEM!
+        ResponseList<Status> stsReply = TWITTER_API.getMentionsTimeline();
+        for (Status stsMention : stsReply) {
+            try {
+                TWITTER_API.createFavorite(stsMention.getId());
+            } catch (TwitterException e) {
+            }
+        }
     }
 
     public static Twitter configureTwitterAuth() {
